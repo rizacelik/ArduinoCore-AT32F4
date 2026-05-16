7ff6ac40b14f87ecb5ba96a710f3863accfa7b70
Rıza Çelik <rizacelik@gmail.com>
2026-05-16 12:02:22 +0300

    Initialize SPI for MPU9250

--- libraries/Generic_Examples/examples/SPI/MPU9250/MPU9250_SPI.ino
+++ libraries/Generic_Examples/examples/SPI/MPU9250/MPU9250_SPI.ino
@@ -16,6 +16,8 @@
 // Chip Select (CS) Pini
 const uint8_t MPU9250_CS_PIN = PA4;
 
+SPIClass SPI(SPI1);
+
 // SPI transfer fonksiyonu (Read/Write bit dahil)
 uint8_t spiTransfer(uint8_t reg, uint8_t data, bool read = false) {
     uint8_t result;
