c54461ee670a56cb886982d05d3c324bfae5a7c2
Rıza Çelik <rizacelik@gmail.com>
2026-05-17 14:07:19 +0300

    Change MPU9250 CS pin and SPI settings

--- libraries/Generic_Examples/examples/SPI/MPU9250/MPU9250_SPI_MadgwickFilter.ino
+++ libraries/Generic_Examples/examples/SPI/MPU9250/MPU9250_SPI_MadgwickFilter.ino
@@ -23,11 +23,10 @@ float pitchGyro = 0, rollGyro = 0, yawGyro = 0;
 unsigned long lastTime = 0;
 
 // Chip Select (CS) Pini
-const uint8_t MPU9250_CS_PIN = PA4;
+const uint8_t MPU9250_CS_PIN = PB12;
+// SPI2 Setting
+SPIClass SPI(SPI2);
 
-#define SPI_SELECT  SPI1
-// #define SPI_SELECT  SPI2
-// #define SPI_SELECT  SPI3
 
 // SPI transfer fonksiyonu (Read/Write bit dahil)
 uint8_t spiTransfer(uint8_t reg, uint8_t data, bool read = false) {
